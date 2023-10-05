8a96ac9715293ee699733ec570a62ef3b79540c7
Zainab Mutalimova <zainab.mutalimova97@gmail.com>
2023-10-05 06:12:28 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int NaturalNumber(int n)
+{
+  if (n != 1)
+  {
+    Console.Write(n + ", ");
+    NaturalNumber(--n);
+  }
+  return 1;
+}
+
+int n = Prompt("Ввведите число N: ");
+if (n < 1)
+{
+  Console.WriteLine("Введите положительное число!");
+  return;
+}
+Console.WriteLine(NaturalNumber(n));
---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int SumOfNaturalNumbers(int m, int n)
+{
+    if (m != n) return SumOfNaturalNumbers(m+1, n) + m;
+    else return m;
+}
+
+int m = Prompt("Ввведите нижнюю границу предела: ");
+int n = Prompt("Ввведите верхнюю границу предела: ");
+Console.WriteLine($"Сумма натуральных чисел предела: {SumOfNaturalNumbers(m, n)}.");
